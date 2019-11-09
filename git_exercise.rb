8a27cb45239c25301f70c4b9c39945f8ab04aa48
= <=>
2019-11-09 15:07:41 +1000

    Final details added and additional methods

--- git_exercise.rb
+++ git_exercise.rb
@@ -1,46 +1,74 @@
-# I want to create a calculator that I can acess in the command line
-
-#  the calculator should give me a list of options I can add the choice to quit
-
-# the calculator should take two values sum, 
-
-
-
-quit = false
-until quit
-    puts "options"
+# I want to create a calculator that I can access in the command line
+# The calculator should give me a list of options I can do and the choice to quit
+# The calculator should take two values sum, subtract, divide, multiply, square (exponent) them.
+# The calculator should take floats and integers, and strings.
+# The calculator should also take a list of values and add, subtract, divide and multiply them together.
+# The calculator's square function should now work with all Exponents
+# Log each calculation and its result into a text file
+def get_user_input
+    puts "Whats your first number?"
+    num1 = gets.chomp.to_i
+    puts "What's your second number?"
+    num2 = gets.chomp.to_i
+    return num1, num2
+  end
+  def sum
+    num1, num2 = get_user_input()
+    result = num1 + num2
+    puts result
+  end
+  def subtract
+    num1, num2 = get_user_input()
+    result = num1 - num2
+    puts result
+  end
+  def divide
+    num1, num2 = get_user_input()
+    result = num1 - num2
+    puts result
+  end
+  def multiply
+    num1, num2 = get_user_input()
+    result = num1 - num2
+    puts result
+  end
+  quit = false
+  until quit do
+    puts "Options"
     puts "1. Sum"
     puts "2. Subtract"
     puts "3. Divide"
     puts "4. Multiply"
     puts "5. Square"
     puts "q. Quit"
-
-    user_input = get.chomp
+    user_input = gets.chomp
     case user_input
     when "1"
-        puts "What's your first number?"
-        num1 = gets.chomp.to_i
-        puts "What's your second number?"
-        num2 = gets.chomp.to_i
-        result = num1 + num2
-        puts result 
-
+      sum()
+      result = num1 + num2
+      puts result
+    when "2"
+      get_user_input()
+      result = num1 - num2
+      puts result
+    when "3"
+      divide()
+      result = num1 / num2
+      puts result
+    when "4"
+      multiply()
+      result = num1 * num2
+      puts result
     when "q"
-        quit = true
+      quit = true
     end
-end
+  end
+  # Requirements 
+  #   takes to values:
+  #     divide
+  #     multiply
+  #     square
+  
 
-
-
-
-Requirement
-    loop
-    print options
-    be able to quit
-    takes to values:
-        sum
-        subtract
-        divide
-        multiply
-        square
+  
+  
