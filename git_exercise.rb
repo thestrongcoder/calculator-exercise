409cadd5871ee79940d4f44834fa85c7b63ebb63
= <=>
2019-11-09 14:36:56 +1000

    my first commit

---
+++ git_exercise.rb
@@ -0,0 +1,28 @@
+# I want to create a calculator that I can acess in the command line
+
+#  the calculator should give me a list of options I can add the choice to quit
+
+# the calculator should take two values sum, 
+
+
+loop do
+    puts "options"
+    puts "1. Sum"
+    puts "2. Subtract"
+    puts "3. Divide"
+    puts "4. Multiply"
+    puts "5. Square"
+    puts "q. Quit"
+end
+
+
+Requirement
+    loop
+    print options
+    be able to quit
+    takes to values:
+        sum
+        subtract
+        divide
+        multiply
+        square
